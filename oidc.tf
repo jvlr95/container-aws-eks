b0f99ad4d492f8fee484c43e21eb6ad9c03d0371
jvlr9510 <jvitor.lr95@hotmail.com>
2025-12-01 01:35:23 -0300

    set oidc config

---
+++ oidc.tf
@@ -0,0 +1,13 @@
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+  tags = {
+    Name = "${var.project_name}-eks-oidc"
+  }
+}
