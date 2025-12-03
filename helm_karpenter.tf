23dcd7600ca38502b4d6563e149039be600dc036
jvlr9510 <jvitor.lr95@hotmail.com>
2025-12-03 00:23:28 -0300

    set config karpenter test

---
+++ helm_karpenter.tf
@@ -0,0 +1,33 @@
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = "karpenter"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+  version    = "1.8.1"
+
+  set = [
+    {
+        name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+        value = aws_iam_role.karpenter.arn
+    },
+    {
+        name  = "settings.clusterName"
+        value = var.project_name
+    },
+    {
+        name  = "settings.clusterEndpoint"
+        value = aws_eks_cluster.main.endpoint
+    },
+    {
+        name  = "aws.defaultInstanceProfile"
+        value = aws_iam_instance_profile.nodes.name
+    }
+  ]
+
+  depends_on = [
+    aws_eks_cluster.main,
+    aws_eks_node_group.main
+  ]
+}
