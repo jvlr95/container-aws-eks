9b8bca4370238ec7e09eb66ee840fa5a6d7fcc57
jvlr9510 <jvitor.lr95@hotmail.com>
2025-12-05 00:54:35 -0300

    create new envs for template/files config karpenter

--- variables.tf
+++ variables.tf
@@ -46,3 +46,16 @@ variable "nodes_instances_sizes" {
   description = "Node instance size"
   type        = list(string)
 }
+
+variable "karpenter_capacity" {
+  type = list(object({
+    name               = string
+    workload           = string
+    ami_family         = string
+    ami_ssm            = string
+    instance_family    = list(string)
+    instance_sizes     = list(string)
+    capacity_type      = list(string)
+    availability_zones = list(string)
+  }))
+}
