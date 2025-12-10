859728c1d9b0fae9406bf76f6edf0f963e401839
jvlr9510 <jvitor.lr95@hotmail.com>
2025-12-10 00:51:17 -0300

    disabled resources monitoring

--- efs_grafana.tf
+++ efs_grafana.tf
@@ -1,33 +1,33 @@
-# resource "aws_efs_file_system" "prometheus" {
-#   creation_token   = format("%s-efs-prometheus", var.project_name)
+# resource "aws_efs_file_system" "grafana" {
+#   creation_token   = format("%s-efs-grafana", var.project_name)
 #   performance_mode = "generalPurpose"
 
 #   tags = {
-#     Name = format("%s-efs-prometheus", var.project_name)
+#     Name = format("%s-efs-grafana", var.project_name)
 #   }
 # }
 
-# resource "aws_efs_mount_target" "prometheus" {
+# resource "aws_efs_mount_target" "grafana" {
 #   count = length(data.aws_ssm_parameter.pod_subnets)
 
 
-#   file_system_id = aws_efs_file_system.prometheus.id
+#   file_system_id = aws_efs_file_system.grafana.id
 #   subnet_id      = data.aws_ssm_parameter.pod_subnets[count.index].value
 #   security_groups = [
 #     aws_security_group.efs.id
 #   ]
 # }
 
-# resource "kubectl_manifest" "prometheus_efs_storage_class" {
+# resource "kubectl_manifest" "grafana_efs_storage_class" {
 #   yaml_body = <<YAML
 # apiVersion: storage.k8s.io/v1
 # kind: StorageClass
 # metadata:
-#   name: efs-prometheus
+#   name: efs-grafana
 # provisioner: efs.csi.aws.com
 # parameters:
 #   provisioningMode: efs-ap
-#   fileSystemId: ${aws_efs_file_system.prometheus.id}
+#   fileSystemId: ${aws_efs_file_system.grafana.id}
 #   directoryPerms: "777"
 # reclaimPolicy: Retain
 # volumeBindingMode: WaitForFirstConsumer
